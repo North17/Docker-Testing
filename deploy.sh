05657d6a1ca4581716a8ff3263ebe93296156546
Marc Stanley <marc.stan17@gmail.com>
2024-04-25 12:20:06 +0530

    final modifications to echos

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 CYAN='\033[0;36m'
-echo "${CYAN}\nStopping currently running project (if running)...\n"
+NC='\033[0m'
+echo "${CYAN}\nStopping currently running project (if running)...\n{$NC}"
 docker-compose -f ./docker-testing/docker-compose.yml -p front_project down
-echo "${CYAN}\nPulling from repository...\n"
+echo "${CYAN}\nPulling from repository...\n{$NC}"
 git pull origin main
-echo "${CYAN}\nStarting up project...\n"
+echo "${CYAN}\nStarting up project...\n{$NC}"
 docker-compose -f ./docker-testing/docker-compose.yml -p front_project up -d
-echo "${CYAN}\nCurrently running containers:\n"
+echo "${CYAN}\nCurrently running containers:\n{$NC}"
 docker ps
--- stop.sh
+++ stop.sh
@@ -1,3 +1,4 @@
 CYAN='\033[0;36m'
-echo "${CYAN}Stopping currently running project (if any)...\n"
+NC='\033[0m'
+echo "${CYAN}Stopping currently running project (if any)...\n{$NC}"
 docker-compose -f ./docker-testing/docker-compose.yml -p front_project down
